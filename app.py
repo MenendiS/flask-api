9926e0022608a78e6929b77ab54c491dcda6f2cd
MenendiS <49616648+MenendiS@users.noreply.github.com>
2024-12-13 11:17:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # In-memory data store
-# Test
 items = [
     {"id": 1, "name": "Item One", "description": "This is item one"},
     {"id": 2, "name": "Item Two", "description": "This is item two"},
